b7a3bbe689606e395c412708952a93cb13faee8f
morde <morde.tadela@gmail.com>
2026-05-07 13:28:05 +0300

    Clean node_modules

---
+++ node_modules/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static(__dirname));
+
+io.on("connection", (socket) => {
+
+    console.log("User connected");
+
+    socket.on("message", (data) => {
+        io.emit("message", data);
+    });
+
+});
+
+http.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
